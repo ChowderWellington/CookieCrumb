e38c278f91ed66be7d5ecba65d55aada440109dd
Cool-Pancakes <97719456+3DGameMaker@users.noreply.github.com>
2024-11-14 16:34:54 +1300

    a vs thingy
    old

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace CookieCrumb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.FormTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 554);
+            this.Font = new System.Drawing.Font("MV Boli", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "CookieCrumb";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MP);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MD);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MM);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MU);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookieCrumb
+{
+    public partial class Form1 : Form
+    {
+
+        Image test;
+        Point position = new Point(200, 200);
+        bool dragging;
+        Rectangle rect;
+        int height = 200, width = 200;
+        public Form1()
+        {
+            test = Image.FromFile("test.png");
+            rect = new Rectangle(position.X,position.Y, width, height);
+            InitializeComponent();
+        }
+
+        private void FormTimerEvent(object sender, EventArgs e)
+        {
+            rect.X = position.X;
+            rect.Y = position.Y;
+            this.Invalidate();
+        }
+
+        private void MD(object sender, MouseEventArgs e)
+        {
+            Point mousePostion = new Point(e.X, e.Y);
+            if (rect.Contains(mousePostion))
+            {
+                dragging = true;
+            }
+        }
+
+        private void MM(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                position = e.Location;
+            }
+        }
+
+        private void MU(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                dragging = false;
+                rect.Location = new Point(e.X, e.Y);
+            }
+        }
+
+        private void MP(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(test, position.X, position.Y, width, height);
+        }
+    }
+}
